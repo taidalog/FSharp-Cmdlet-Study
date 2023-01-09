4bc857212687e31221463c6fe4e97aaf074da37f
taidalog <72747279+taidalog@users.noreply.github.com>
2023-01-09 22:23:46 +0900

    Create a new solution and a project. Close #1.

---
+++ src/FSharpCmdletStudy/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharpCmdletStudy
+
+open System
+open System.Management.Automation
+
+[<Cmdlet(VerbsData.Out, "Twice")>]
+type OutTwiceCommand () =
+    inherit PSCmdlet ()
+
+    [<Parameter>]
+    member val InputString : string = "" with get, set
+
+    override x.EndProcessing () =
+        sprintf "%s %s" x.InputString x.InputString
+        |> x.WriteObject
+        base.EndProcessing ()
