507b73a11d0fb947676cc43577aefa140833e280
taidalog <72747279+taidalog@users.noreply.github.com>
2023-01-09 23:02:56 +0900

    Added a script for Step 2. #3

---
+++ steps/step2/step2.ps1
@@ -0,0 +1,41 @@
+$slnName = "FSharpCmdletStudy"
+cd $slnName
+
+@"
+namespace $slnName
+
+open System
+open System.IO
+open System.Management.Automation
+
+[<Cmdlet(VerbsCommon.New, "Person")>]
+type NewPersonCommand () =
+    inherit PSCmdlet ()
+
+    [<Parameter(Mandatory = true, Position = 0)>]
+    member val FamiryName : string = "" with get, set
+    
+    [<Parameter(Mandatory = true, Position = 1)>]
+    member val FirstName : string = "" with get, set
+    
+    [<Parameter(Mandatory = true, Position = 0)>]
+    member val Language : string = "" with get, set
+
+    override this.EndProcessing () =
+        sprintf "%s %s uses %s." this.FamiryName this.FirstName this.Language 
+        |> this.WriteObject
+        base.EndProcessing ()
+"@ | Out-File ./src/$slnName/Step2.fs -Encoding utf8
+
+# Add `<Compile Include="Step2.fs" />` to `<ItemGroup>` in `.fsproj` file MANUALLY.
+# Then `<ItemGroup>` in `.fsproj` will be as below:
+#  <ItemGroup>
+#  <Compile Include="Library.fs" />
+#  <Compile Include="Step2.fs" />
+#  </ItemGroup>
+
+# After editing, you can go on.
+
+dotnet build
+dotnet publish
+Import-Module ./src/$slnName/bin/Debug/netstandard2.0/publish/$slnName.dll
